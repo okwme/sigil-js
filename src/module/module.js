0a62e8047c79a92c6067d00d81cd7bb05fb2c795
Anthony Arroyo <anthony@tlon.io>
2018-09-06 15:06:49 -0700

    add plain svg renderer to module

--- src/module/module.js
+++ src/module/module.js
@@ -1,4 +1,5 @@
 import { pour } from '../lib/pour'
+import SVGComponents from '../renderers/SVGComponents'
 import sylmap from '../sylmaps/sylmap'
 
-export { pour, sylmap }
+export { pour, sylmap, SVGComponents }
